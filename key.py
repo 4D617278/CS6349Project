fa4ad3cd9d99d0e49221bde12f8b4da5e1a06f9c
user <bxl190001@utdallas.edu>
2022-11-28 12:23:08 -0600

    add key generator

---
+++ key.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from nacl.encoding import HexEncoder
+from nacl.public import PrivateKey
+from sys import argv
+
+def main():
+    if len(argv) < 3: 
+        print(f'usage: {argv[0]} <numClients> <path>')
+        exit(1)
+
+    try:
+        numClients = int(argv[1])
+    except ValueError:
+        print('number of clients is not integer')
+        exit(1)
+
+    if numClients <= 0:
+        print('number of clients is not positive')
+        exit(1)
+
+    hex_keys = [None] * numClients
+
+    for i in range(numClients):
+        sk = PrivateKey.generate()
+        hex_keys[i] = sk.encode(HexEncoder) + b'\n'
+
+    with open(argv[2], 'wb+') as f:
+        f.writelines(hex_keys)
+
+if __name__ == '__main__':
+    main()
