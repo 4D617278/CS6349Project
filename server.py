07e28d0bd7e6c51323f61fd71dadd8fcf8d70cc0
user <bxl190001@utdallas.edu>
2022-11-28 14:02:32 -0600

    send nonce from server, generalize argument handler

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from enum import IntEnum
+from nacl.public import Box 
+from nacl.utils import random
 import socket
 from sys import argv
 
@@ -6,26 +9,35 @@ HOST = '0.0.0.0'
 MIN_PORT = 1024
 MAX_PORT = 65535
 
+class Args(IntEnum):
+    port = 1
+    keys = 2
+
 def main():
-    if len(argv) < 2: 
-        print(f'usage: {argv[0]} <port>')
+    if len(argv) < len(Args): 
+        args = ' '.join(f'<{a.name}>' for a in Args)
+        print(f'usage: {argv[0]} {args}')
         exit(1)
 
     try:
-        port = int(argv[1])
+        port = int(argv[Args.port])
     except ValueError:
         print('port is not integer')
         exit(1)
 
     if port < MIN_PORT or MAX_PORT < port:
-        print('MIN_PORT <= port <= MAX_PORT')
+        print(f'{MIN_PORT} <= port <= {MAX_PORT}')
         exit(1)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, port))
     s.listen()
-    s.accept()
+    conn, addr = s.accept()
 
+    nonce = random(Box.NONCE_SIZE)
+
+    # challenge
+    conn.send(nonce)
 
 if __name__ == '__main__':
     main()
