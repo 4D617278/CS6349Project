f83ae38cfd303f1d3adfba2bc755fe60109c5d8a
Jerry Teng <zuyuangteng@gmail.com>
2022-11-30 17:20:46 -0600

    implement hmac-256 in cfb mode

--- utility.py
+++ utility.py
@@ -2,11 +2,16 @@ import argparse
 
 from nacl.hash import sha256
 from nacl.utils import random
+from nacl.encoding import RawEncoder
 
 from config import SIGNATURE_SIZE, HASH_OUTPUT_SIZE
 
 MIN_PORT = 0
 MAX_PORT = 65535
+
+
+def raw_sha256(message):
+    return sha256(message, encoder=RawEncoder)
 
 def allowed_ports(port):
     int_port = int(port)
@@ -14,13 +19,13 @@ def allowed_ports(port):
         return int_port 
     raise argparse.ArgumentTypeError(f"{MIN_PORT} <= port <= {MAX_PORT}")
 
-def sign_hash(message, signing_key, hash_function=sha256):
+def sign_hash(message, signing_key, hash_function=raw_sha256):
     hashed_message = hash_function(message)
     print(f"Hashed message: {hashed_message} of length {len(hashed_message)}")
     signed_message = signing_key.sign(hashed_message)
     return signed_message
 
-def encrypt_and_sign(message, box, signing_key, hash_function=sha256):
+def encrypt_and_sign(message, box, signing_key, hash_function=raw_sha256):
     print(f"Original message: {message} of length {len(message)}")
     encrypted_message = box.encrypt(message)
     print(f"Encrypted message: {encrypted_message} of length {len(encrypted_message)}")
@@ -48,14 +53,14 @@ def pad(key, block_size=64):
     padding = bytearray(block_size - len(key))
     return key + padding
 
-def compute_block_sized_key(key, block_size=HASH_OUTPUT_SIZE, hash_function=sha256):
+def compute_block_sized_key(key, block_size=HASH_OUTPUT_SIZE, hash_function=raw_sha256):
     if len(key) > block_size:
         key = hash_function(key)
     if len(key) < block_size:
         return pad(key, block_size)
     return key
 
-def hmac(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=sha256):
+def hmac(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=raw_sha256):
     block_sized_key = compute_block_sized_key(key)
     opad = b"\x5c" * block_size
     ipad = b"\x36" * block_size
@@ -63,44 +68,52 @@ def hmac(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=sha256):
     i_key_pad = xor(block_sized_key, ipad)
     return hash_function(o_key_pad + hash_function(i_key_pad + message))
 
-def keyed_hash_encryption(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=sha256):
+def keyed_hash_encryption(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=raw_sha256):
     # Encryption using HMAC-256 keystream in cipher feedback mode
     key_byte_arr = bytearray(key)
     message_byte_arr = bytearray(message)
+    # print(f"Length of plaintext: {len(message_byte_arr)}")
     iv = hash_function(key)
     output = key_byte_arr
     encrypted = bytearray()
     prev_enc = iv
-    for i in range(0, len(message_byte_arr), 64):
-        block = message_byte_arr[i:i+64]
+    for i in range(0, len(message_byte_arr), HASH_OUTPUT_SIZE):
+        block = message_byte_arr[i:i+HASH_OUTPUT_SIZE]
         output = hmac(iv, prev_enc)
-        enc = xor(block, hmac(iv, prev_enc))
-        print(enc)
+        enc = xor(block, output)
         prev_enc = enc
         encrypted += enc
+        # print(f"Block offset: {i}:\nplaintext: {block}\noutput: {output}\nciphertext: {enc}\n")
+    # print(f"Length of encrypted value: {len(encrypted)}")
     return encrypted
 
-def keyed_hash_decryption(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=sha256):
+def keyed_hash_decryption(key, message, block_size=HASH_OUTPUT_SIZE, hash_function=raw_sha256):
     # Decryption using HMAC-256 keystream in cipher feedback mode
     key_byte_arr = bytearray(key)
     message_byte_arr = bytearray(message)
+    # print(f"Length of ciphertext: {len(message_byte_arr)}")
     iv = hash_function(key)
     output = key_byte_arr
     decrypted = bytearray()
     prev_dec = iv
-    for i in range(0, len(message_byte_arr), 64):
-        block = message_byte_arr[i:i+64]
-        dec = xor(block, hmac(iv, prev_dec))
-        print(dec)
-        prev_dec = dec
+    for i in range(0, len(message_byte_arr), HASH_OUTPUT_SIZE):
+        block = message_byte_arr[i:i+HASH_OUTPUT_SIZE]
+        output = hmac(iv, prev_dec)
+        dec = xor(block, output)
+        prev_dec = block
         decrypted += dec
+        # print(f"Block offset: {i}:\nciphertext: {block}\noutput: {output}\nplaintext: {dec}\n")
+    # print(f"Length of decrypted value: {len(decrypted)}")
     return decrypted
 
 if __name__ == "__main__":
     secret_key = random(32)
-    message = b"A" * 64 * 2
+    # message = b"A" * 64
+    message = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus sed viverra tellus in hac habitasse platea. Pulvinar pellentesque habitant morbi tristique senectus et netus. Aenean vel elit scelerisque mauris pellentesque. Iaculis eu non diam phasellus vestibulum lorem sed."
+    print(f"Plaintext: {message}")
+    print("*" * 20)
     enc = keyed_hash_encryption(secret_key, message)
-    # print(enc)
+    print(f"Encrypted: {enc}")
     print("*" * 20)
     dec = keyed_hash_decryption(secret_key, enc)
-    # print(dec)
+    print(f"Decrypted: {dec}")
